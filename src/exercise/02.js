2d332eac04872f15a560f3cb2b0167c90b931bd6
Mike Codeur <cabanas.mickael@gmail.com>
2022-01-06 10:45:16 +0800

    fix issue 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,46 +3,35 @@
 
 import * as React from 'react'
 
-function Calcul({nb1, nb2, operation}) {
-  const [resultat, setResultat] = React.useState()
+// 🐶 Créé 3 props du composant Calcul : 'nb1', 'nb2' pour les nombres à calculer
+// et le prop opération qui contiendra la fonction de calcul
+// 🤖 utilise la destruction `Calcul({nb1, nb2, operation})`
 
-  const [nombre1, setNombre1] = React.useState(nb1)
-  const [nombre2, setNombre2] = React.useState(nb2)
+function Calcul() {
+  // 🐶 Calcule le résulat de l'opération sur 'nb1' et 'nb2'
+  // 🤖 operation(nb1, nb2)
 
-  const opName = operation.name
-  const handleClick = (a = nb1, b = nb2) => {
-    if (a instanceof Object) {
-      a = nb1
-    }
-    setNombre1(a)
-    setNombre2(b)
-    setResultat(operation(a, b))
-  }
-  return (
-    <div>
-      <input
-        type="button"
-        onClick={() => handleClick(50, 50)}
-        value={`Calculer ${opName} 50 50`}
-      />
-      <input type="button" onClick={handleClick} value={`Calculer ${opName}`} />
-      {resultat
-        ? `L'opération ${opName} de ${nombre1} et ${nombre2} donne ${resultat}`
-        : null}
-    </div>
-  )
+  // 🐶 Récupère le nom de l'opération via à la propriété 'name' de 'function'
+  // 🤖 `operation.name`
+  // 📑 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+
+  // 🐶 Modifie la chaine de caractère ci-dessous pour la rendre dynamique
+  return <div>L'opération somme de 5 et 10 donne 15</div>
 }
 
 function App() {
-  const somme = (a, b) => a + b
-  const multiplication = (a, b) => a * b
-  const exposant = (a, b) => a * Math.exp(b)
+  // 🐶 Déclare une fonction 'somme' et passe la en prop de '<Calcul>'
+  // 🤖 <Calcul operation={somme} />
 
+  // 🐶 Déclare une fonction 'multiplication' et passe la en prop de '<Calcul>'
+
+  // 🐶 Déclare une fonction 'exposant' et passe la en prop de '<Calcul>'
+  // 🤖 utilise `a * Math.exp(b)` pour calculer 'a exposant b'
   return (
     <>
-      <Calcul nb1={5} nb2={10} operation={somme}></Calcul>
-      <Calcul nb1={5} nb2={10} operation={multiplication}></Calcul>
-      <Calcul nb1={5} nb2={10} operation={exposant}></Calcul>
+      <Calcul></Calcul>
+      <Calcul></Calcul>
+      <Calcul></Calcul>
     </>
   )
 }
